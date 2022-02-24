5a9dc86525a7277494e6c415b880576614cd4fa0
declancm <declanmullen3.14@gmail.com>
2022-02-25 01:17:55 +1100

    Auto Commit: Feb 24 14:17:55 2022 UTC

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -11,7 +11,7 @@ if git rev-parse --git-dir > /dev/null 2>&1; then
     git add .
     wait
     # git commit -a -m "Auto Commit: `date -u +'%Y-%m-%d %H:%M:%S'` UTC"
-    git commit -a -m "Auto Commit: `date -u +'%a %b %H:%M:%S %Y'` UTC"
+    git commit -a -m "Auto Commit: `date -u +'%b %d %H:%M:%S %Y'` UTC"
     wait
     git push $remote $branch
     cd $OLDPWD
